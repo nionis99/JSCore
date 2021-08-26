8f490a8715beb8d1525433c00c3218baaf57eab0
Ignas Stanionis <nionis0697@gmail.com>
2021-08-26 19:43:47 +0300

    wip

--- events/events.js
+++ events/events.js
@@ -1,11 +1,11 @@
 function createMatrixTable(tableSize) {
     let tableContainer = "";
-    tableContainer += "<table>";
+    tableContainer += "<table id='table'>";
 
     for (let i = 1; i <= tableSize; i++) {
         tableContainer += "<tr>";
         for (let j = 1; j <= tableSize; j++) {
-            tableContainer += "<td>" + (i * j) + "\n</td>";
+            tableContainer += `<td id="${i * j}">` + (i * j) + "\n</td>";
         }
         tableContainer += "</tr>";
     }
@@ -16,3 +16,7 @@ function createMatrixTable(tableSize) {
 
 
 createMatrixTable(100);
+
+document.getElementById('table').addEventListener('click', (event) => {
+    alert(`ID: ${event.target.id} Value: ${event.target.innerHTML}`);
+})
