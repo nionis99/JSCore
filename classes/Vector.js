47052601bdeb04d1226b2338e5a247ce16c53641
live-mi <88105444+live-mi@users.noreply.github.com>
2021-08-26 11:47:08 +0300

    Add comments and suggestions

--- classes/Vector.js
+++ classes/Vector.js
@@ -1,10 +1,13 @@
 function Vector(values) {
     this.values = values;
 
+    // I would name the method ```isLengthsEqual``` because you are comparing lengths of the vectors.
     function isEquals(vector) {
         return vector.values.length === values.length
     }
 
+    // Could you please separate this function into 2: checkCompatibility and calculate to follow by single responsibility principle (https://en.wikipedia.org/wiki/Single-responsibility_principle)
+    // Additionally the initial letter shoul be in lowercase for usual functions.
     function CheckAndCalculate(vector, fn) {
         if (isEquals(vector)) {
             return values.map(fn)
@@ -18,7 +21,7 @@ function Vector(values) {
         substract: (vector) => CheckAndCalculate(vector, (num, id) => num - vector.values[id]),
         dot: (vector) => CheckAndCalculate(vector, (num, id) => num * vector.values[id]),
         norm: (fixedNumbers) => Math.sqrt(values.reduce((sum, a) => sum + Math.pow(a, 2), 0)).toFixed(fixedNumbers),
-        toString: () => values.toString(),
+        toString: () => values.toString(), // ```toString``` method should return string in the pattern like this: '(1,2,3)'
         get values() {
             return values;
         },
