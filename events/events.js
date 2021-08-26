0c6fadabd1bb310659717629290403d361767f90
Ignas Stanionis <nionis0697@gmail.com>
2021-08-26 13:22:10 +0300

    WIP

---
+++ events/events.js
@@ -0,0 +1,17 @@
+function createMatrixTable(tableSize) {
+    let tableContainer = "";
+    tableContainer += "<table>";
+
+    for (let i = 1; i <= tableSize; i++) {
+        tableContainer += "<tr>";
+        for (let j = 1; j <= tableSize; j++) {
+            tableContainer += "<td>" + (i * j) + "\n</td>";
+        }
+        tableContainer += "</tr>";
+    }
+    tableContainer += "</table>";
+
+    return document.write(tableContainer);
+}
+
+createMatrixTable(100);
