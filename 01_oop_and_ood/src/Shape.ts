b761d22306e99825997ff94102e3547f8ed0b892
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-09-08 09:39:06 +0300

    shape

--- 01_oop_and_ood/src/Shape.ts
+++ 01_oop_and_ood/src/Shape.ts
@@ -1,3 +1,33 @@
+import {Point} from "./Point";
+
 export abstract class Shape {
+    protected points: Point[];
+    protected color?: string;
+    protected isFilled?: boolean;
+
+    constructor(points: Point[]);
+    constructor(points: Point[], color?: string, isFilled?: boolean) {
+        if (points.length >= 3) {
+            this.color = color || "green";
+            this.isFilled = isFilled !== undefined ? isFilled : true;
+            this.points = points
+        } else {
+            throw new Error("Shape must have 3 points at least")
+        }
+    }
+
+    public toString() {
+        return `A Shape with color of ${this.color} and ${this.isFilled ? 'filled' : 'not filled'}. Points: ${this.points.join(', ').toString()}.`;
+    }
+
+    public getPerimeter() {
+        let perimeter = 0;
+        this.points.forEach((point, index, points) => {
+            const distance = point.distance(points[index + 1 != points.length ? index + 1 : 0])
+            perimeter += distance;
+        });
+        return perimeter;
+    }
+
     abstract getType(): string;
 }
