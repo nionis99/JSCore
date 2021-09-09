5abb3774c358c86e93a09fd3c3b859f494367da4
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-09-09 09:52:14 +0300

    created custom error

--- errors/error.js
+++ errors/error.js
@@ -1,5 +1,12 @@
+class NamedError extends Error {
+    constructor(name, message) {
+        super(message);
+        this.name = name;
+    }
+}
+
 try {
     console.show("This won't work");
 } catch ({message}) {
-    throw new Error(message);
+    throw new NamedError("Console error: ", message);
 }
